1d5565857ef83c949b0f788b28585039349e6191
Daniel Burgener <dburgener@tresys.com>
2018-11-16 15:40:21 -0500

    Reorder command line arguments in code

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <getopt.h>
 #include "tree.h"
 #include "parse.h"
+#include "config.h"
 
 extern int yyparse();
 
@@ -13,15 +14,16 @@ int main(int argc, char **argv) {
 
 		static struct option long_options[] = 
 			{
-				{ "version", no_argument, NULL, 'V' },
-				{ "verbose", no_argument, &verbose_flag, 1 },
-				{ "help", no_argument, NULL, 'h' },
-				{ "recursive", no_argument, NULL, 'r' },
 				{ "config", required_argument, NULL, 'c' },
-				{ "modules-conf", required_argument, NULL, 'm' },
 				{ "disable", required_argument, NULL, 'd' },
 				{ "enable", required_argument, NULL, 'e' },
+				{ "help", no_argument, NULL, 'h' },
 				{ "level", required_argument, NULL, 'l' },
+				{ "modules-conf", required_argument, NULL, 'm' },
+				{ "recursive", no_argument, NULL, 'r' },
+				{ "source", no_argument, NULL, 's' },
+				{ "version", no_argument, NULL, 'V' },
+				{ "verbose", no_argument, &verbose_flag, 1 },
 				{ 0, 0, 0, 0 }
 			};
 
@@ -35,27 +37,12 @@ int main(int argc, char **argv) {
 
 		switch (c) {
 
+			//getopt returns 0 when a long option with no short equivalent is used
 			case 0:
-				break;
-
-			case 'V':
-				printf("Version\n");
-				break;
-
-			case 'v':
-				verbose_flag = 1;
-				break;
-
-			case 'r':
-				printf("recursive\n");
 				break;
 
 			case 'c':
 				printf("Config file: %s\n", optarg);
-				break;
-
-			case 'm':
-				printf("Flag m with value %s\n", optarg);
 				break;
 
 			case 'd':
@@ -68,6 +55,26 @@ int main(int argc, char **argv) {
 
 			case 'l':
 				printf("Flag l with value %s\n", optarg);
+				break;
+
+			case 'm':
+				printf("Flag m with value %s\n", optarg);
+				break;
+
+			case 'r':
+				printf("recursive\n");
+				break;
+
+			case 's':
+				printf("source mode\n");
+				break;
+
+			case 'V':
+				printf("Version %s\n", VERSION);
+				break;
+
+			case 'v':
+				verbose_flag = 1;
 				break;
 
 			case '?':
