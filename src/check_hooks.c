6d4cabd5ecc241d4499d337d82013a9d44bb5609
Daniel Burgener <dburgener@tresys.com>
2019-01-17 12:48:36 -0500

    Include the line number in results

--- src/check_hooks.c
+++ src/check_hooks.c
@@ -56,6 +56,7 @@ enum selint_error call_checks_for_node_type(struct check_node *ck_list, struct c
 	while (cur) {
 		struct check_result *res = cur->check_function(data, node);
 		if (res) {
+			res->lineno = node->lineno;
 			display_check_result(res, data);
 			free_check_result(res);
 		}
