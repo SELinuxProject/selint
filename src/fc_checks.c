f24c92543457e90b5a977c88bc896d14fa35cccd
Daniel Burgener <dburgener@tresys.com>
2019-01-18 08:20:42 -0500

    Use consistent and accurate variable naming in fc check in mod

--- src/fc_checks.c
+++ src/fc_checks.c
@@ -54,9 +54,9 @@ struct check_result *check_file_context_types_in_mod(const struct check_data *ch
 		return alloc_internal_error("Policy node data field is NULL");
 	}
 
-	char *type_decl_filename = look_up_in_decl_map(entry->context->type, DECL_TYPE);
+	char *type_decl_mod_name = look_up_in_decl_map(entry->context->type, DECL_TYPE);
 
-	if (!type_decl_filename) {
+	if (!type_decl_mod_name) {
 		// If the type is not in any module, that's a different error
 		// Returning success on an error condition may seem weird, but it is a
 		// redundant condition with another check that will catch this if enabled.
@@ -68,12 +68,12 @@ struct check_result *check_file_context_types_in_mod(const struct check_data *ch
 		return NULL;
 	}
 
-	if (strcmp(check_data->mod_name, type_decl_filename)) {
+	if (strcmp(check_data->mod_name, type_decl_mod_name)) {
 		struct check_result *res = malloc(sizeof(struct check_result));
 
 		res->severity = 'S';
 		res->check_id = S_ID_FC_TYPE;
-		if (!asprintf(&res->message, "Type %s is declared in module %s, but used in file context here.", entry->context->type, type_decl_filename)) {
+		if (!asprintf(&res->message, "Type %s is declared in module %s, but used in file context here.", entry->context->type, type_decl_mod_name)) {
 			free(res);
 			return alloc_internal_error("Failed to generate error message in fc type checking");
 		}
