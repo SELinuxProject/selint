f1873223ce2b0ce7a37f6ebc1d5b716e74a12643
Daniel Burgener <dburgener@tresys.com>
2019-01-25 12:33:28 -0500

    Add stubs for interface checks

---
+++ src/if_checks.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+
+#include "if_checks.h"
+
+
+struct check_result *check_interface_definitions_have_comment(const struct check_data *data, const struct policy_node *node) {
+	return NULL;
+}
+
+struct check_result *type_used_but_not_required_in_if(const struct check_data *data, const struct policy_node *node) {
+	return NULL;
+}
---
+++ src/if_checks.h
@@ -0,0 +1,25 @@
+#ifndef IF_CHECKS_H
+#define IF_CHECKS_H
+
+#include "check_hooks.h"
+
+/*********************************************
+ * Check to make sure all interfaces and templates have a comment above them 
+ * Called on NODE_IF_DEF and NODE_TEMP_DEF nodes.
+ * data - metadata about the file
+ * node - the node to check
+ * returns NULL if passed or check_result for issue C-002
+ *********************************************/
+struct check_result *check_interface_definitions_have_comment(const struct check_data *data, const struct policy_node *node);
+
+/*********************************************
+ * Check that all types referenced in interface are listed in its require block
+ * (or declared in that template)
+ * Called on NODE_AV_RULE, NODE_TT_RULE and NODE_IF_CALL nodes.
+ * data - metadata about the file
+ * node - the node to check
+ * returns NULL if passed or check_result for issue W-002
+ *********************************************/
+struct check_result *type_used_but_not_required_in_if(const struct check_data *data, const struct policy_node *node);
+
+#endif
