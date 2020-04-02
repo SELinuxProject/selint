SELinuxProject/selint

commit 3f83758c1825c1e720867e18ab97ee18ef0eadf9

Separate grammar for real declarations and require content

src/parse.y
@@ -261,15 +261,16 @@ bare_line:
 declaration:
 	type_declaration
 	|
-	attribute_declaration
+	ATTRIBUTE STRING SEMICOLON { insert_declaration(&cur, DECL_ATTRIBUTE, $2, NULL, yylineno); free($2); }
 	|
 	CLASS STRING string_list SEMICOLON { free($2); free_string_list($3); }
 	|
 	role_declaration
 	|
-	ATTRIBUTE_ROLE comma_string_list SEMICOLON { free_string_list($2); }
+	// TODO: insert_declaration()
+	ATTRIBUTE_ROLE STRING SEMICOLON { free($2); }
 	|
-	BOOL comma_string_list SEMICOLON { insert_declaration(&cur, DECL_BOOL, NULL, $2, yylineno); }
+	BOOL STRING SEMICOLON { insert_declaration(&cur, DECL_BOOL, $2, NULL, yylineno); free($2); }
 	;
 
 type_declaration:
@@ -288,16 +289,8 @@ type_declaration:
 				insert_aliases(&cur, tmp, DECL_TYPE, yylineno); }
 	;
 
-attribute_declaration:
-	ATTRIBUTE STRING SEMICOLON { insert_declaration(&cur, DECL_ATTRIBUTE, $2, NULL, yylineno); free($2); }
-	|
-	ATTRIBUTE STRING COMMA comma_string_list SEMICOLON { insert_declaration(&cur, DECL_ATTRIBUTE, $2, $4, yylineno); free($2); }
-	;
-
 role_declaration:
 	ROLE STRING SEMICOLON { insert_declaration(&cur, DECL_ROLE, $2, NULL, yylineno); free($2); }
-	|
-	ROLE STRING COMMA comma_string_list SEMICOLON { insert_declaration(&cur, DECL_ROLE, $2, $4, yylineno); free($2); }
 	|
 	ROLE STRING TYPES string_list SEMICOLON { insert_declaration(&cur, DECL_ROLE, $2, $4, yylineno); free($2); }
 	;
@@ -436,26 +429,68 @@ optional_open:
 
 require:
 	gen_require_begin
-	BACKTICK lines SINGLE_QUOTE CLOSE_PAREN { end_gen_require(&cur); }
+	BACKTICK require_lines SINGLE_QUOTE CLOSE_PAREN { end_gen_require(&cur); }
 	|
 	gen_require_begin
-	BACKTICK SELINT_COMMAND lines SINGLE_QUOTE CLOSE_PAREN { end_gen_require(&cur); save_command(cur, $3); free($3); }
+	BACKTICK SELINT_COMMAND require_lines SINGLE_QUOTE CLOSE_PAREN { end_gen_require(&cur); save_command(cur, $3); free($3); }
 	|
 	// TODO: This is bad and should be checked
 	gen_require_begin
-	lines CLOSE_PAREN { end_gen_require(&cur); }
+	require_lines CLOSE_PAREN { end_gen_require(&cur); }
 	|
 	REQUIRE OPEN_CURLY { begin_require(&cur, yylineno); }
-	lines CLOSE_CURLY { end_require(&cur); }
+	require_lines CLOSE_CURLY { end_require(&cur); }
 	|
 	REQUIRE OPEN_CURLY SELINT_COMMAND { begin_require(&cur, yylineno); save_command(cur->parent, $3); }
-	lines CLOSE_CURLY { end_require(&cur); free($3); }
+	require_lines CLOSE_CURLY { end_require(&cur); free($3); }
 	;
 
 gen_require_begin:
 	GEN_REQUIRE OPEN_PAREN { begin_gen_require(&cur, yylineno); }
 	|
 	GEN_REQUIRE OPEN_PAREN SELINT_COMMAND { begin_gen_require(&cur, yylineno); save_command(cur->parent, $3); free($3); }
+	;
+	
+require_lines:
+	require_lines require_line
+	|
+	require_line
+	;
+
+require_line:
+	TYPE comma_string_list SEMICOLON {
+		const struct string_list *iter = $2;
+		for (iter = $2; iter; iter = iter->next) insert_declaration(&cur, DECL_TYPE, iter->string, NULL, yylineno);
+		free_string_list($2);
+		}
+	|
+	ATTRIBUTE comma_string_list SEMICOLON {
+		const struct string_list *iter = $2;
+		for (iter = $2; iter; iter = iter->next) insert_declaration(&cur, DECL_ATTRIBUTE, iter->string, NULL, yylineno);
+		free_string_list($2);
+		}
+	|
+	ROLE comma_string_list SEMICOLON {
+		const struct string_list *iter = $2;
+		for (iter = $2; iter; iter = iter->next) insert_declaration(&cur, DECL_ROLE, iter->string, NULL, yylineno);
+		free_string_list($2);
+		}
+	|
+	ATTRIBUTE_ROLE comma_string_list SEMICOLON {
+		//TODO: const struct string_list *iter = $2;
+		//TODO: for (iter = $2; iter; iter = iter->next) insert_declaration(&cur, DECL_ATRIBUTE_ROLE, iter->string, NULL, yylineno);
+		free_string_list($2);
+		}
+	|
+	BOOL comma_string_list SEMICOLON {
+		const struct string_list *iter = $2;
+		for (iter = $2; iter; iter = iter->next) insert_declaration(&cur, DECL_BOOL, iter->string, NULL, yylineno);
+		free_string_list($2);
+		}
+	|
+	CLASS STRING string_list SEMICOLON { free($2); free_string_list($3); }
+	|
+	COMMENT
 	;
 
 m4_call:

tests/check_parsing.c
@@ -148,10 +148,20 @@ START_TEST (test_parse_basic_if) {
 	current = current->next;
 
 	ck_assert_int_eq(NODE_DECL, current->flavor);
+	ck_assert_str_eq("basic_t", current->data.d_data->name);
 	ck_assert_ptr_null(current->first_child);
-	ck_assert_ptr_null(current->next);
 	ck_assert_ptr_nonnull(current->prev);
 	ck_assert_ptr_nonnull(current->parent);
+	ck_assert_ptr_nonnull(current->next);
+
+	current = current->next;
+
+	ck_assert_int_eq(NODE_DECL, current->flavor);
+	ck_assert_str_eq("basic_exec_t", current->data.d_data->name);
+	ck_assert_ptr_null(current->first_child);
+	ck_assert_ptr_nonnull(current->prev);
+	ck_assert_ptr_nonnull(current->parent);
+	ck_assert_ptr_null(current->next);
 
 	current = current->parent->next;
 
@@ -169,6 +179,7 @@ START_TEST (test_parse_uncommon_constructs) {
 
 	ast = cur = calloc(1, sizeof(struct policy_node));
 	set_current_module_name("uncommon");
+	parsing_filename = "uncommon.te";
 
 	yyin = fopen(UNCOMMON_TE_FILENAME, "r");
 	ck_assert_int_eq(0, yyparse());

commit 388a81b9affe394c12378f48f7c015ef03b4fe7e

Allow selint-disable comment after declaration in require block
Fixes: 3f83758c1825c1e720867e18ab97ee18ef0eadf9 ("Separate grammar for real declarations and require content")

src/parse.y
@@ -469,6 +469,12 @@ require_lines:
 	;
 
 require_line:
+	require_bare
+	|
+	require_bare SELINT_COMMAND { save_command(cur, $2); free($2); }
+	;
+
+require_bare:
 	TYPE comma_string_list SEMICOLON {
 		const struct string_list *iter = $2;
 		for (iter = $2; iter; iter = iter->next) insert_declaration(&cur, DECL_TYPE, iter->string, NULL, yylineno);

tests/Makefile.am
@@ -184,6 +184,7 @@ FUNCTIONAL_TEST_FILES=functional/end-to-end.bats \
 			functional/policies/misc/disable.if \
 			functional/policies/misc/disable_multiple_other.te \
 			functional/policies/misc/disable_multiple.te \
+			functional/policies/misc/disable_require_decl.if \
 			functional/policies/misc/disable_require_start.te \
 			functional/policies/misc/disable.te \
 			functional/policies/misc/needs_context.te \

tests/functional/end-to-end.bats
@@ -272,6 +272,10 @@ test_one_check() {
 	run ${SELINT_PATH} -F -c configs/default.conf -d S-008 policies/misc/disable_require_start.*
 	[ "$status" -eq 0 ]
 	[ "$output" == "" ]
+
+	run ${SELINT_PATH} -F -c configs/default.conf policies/misc/disable_require_decl.*
+	[ "$status" -eq 0 ]
+	[ "$output" == "" ]
 }
 
 @test "nonexistent file" {

tests/functional/policies/misc/disable_require_decl.if
@@ -1,0 +1,6 @@
+#comment
+interface(`disable_require_decl_if',`
+	gen_require(`
+		type foo_t; #selint-disable:W-003
+	')
+')